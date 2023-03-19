d4424e610f547a84e15e7e5cc0385cec31c3b383
igor <igornotfound@gmail.com>
2023-03-19 20:41:10 +0100

    basic google login implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Login from "./components/Login.jsx";
+import Home from "./container/Home.jsx";
+
+import {GoogleOAuthProvider} from "@react-oauth/google";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_APP_PUBLIC_GOOGLE_API_TOKEN}>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </GoogleOAuthProvider>
+
+
+
   );
 };
 
