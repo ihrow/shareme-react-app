8fbd9a135f40c5b66af0c8c2559080f48cd4f878
igor <igornotfound@gmail.com>
2023-03-20 21:21:11 +0100

    search component implemented

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import MasonryLayout from "./MasonryLayout.jsx";
+import {client} from "../client.js";
+import {feedQuery, searchQuery} from "../utils/data.js";
+import Spinner from "./Spinner";
 
-const Search = () => {
+
+const Search = ({ searchTerm }) => {
+  const [pins, setPins] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if(searchTerm) {
+      setLoading(true)
+      const query = searchQuery(searchTerm)
+      client.fetch(query)
+        .then((data) => {
+          setPins(data)
+        })
+        .finally(() => setLoading(false))
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data)
+        setLoading(false)
+      })
+    }
+  }, [searchTerm])
+
   return (
     <div>
-      Search
-
+      {loading && <Spinner message="Searching for pins" />}
+      {pins && pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== '' && !loading
+        && <div className="mt-10 text-center text-xl">No pins found for {searchTerm}</div>
+        }
     </div>
   );
 };
